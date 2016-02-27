cb0e0e60e73ae8bd8c46ee64721c1f2c16e55441
Sreejith S <sreejith.sreekumar@247-inc.com>
2016-02-27 12:28:43 +0530

    add numpy array basics and image dataset reading functions for tensorfow

---
+++ deeplearning/tensorflow/numpy_basics.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+__author__ = 'Sreejith S'
+
+
+
+
+# shuffling a 2d array
+# important - read arrays with float values as float and int values as int
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9],[3,5,7],[5,8,2]])
+print("a initialized with type", type(a))
+
+# creating an ndarray from the array
+#first argument is a tupe with the array dimentions
+b = np.ndarray((len(a), 3), dtype=int, buffer = a)
+
+
+#shuffling the elements in the array
+print("Shuffling elements in array b")
+np.random.shuffle(b)
+print(b)
+ 
