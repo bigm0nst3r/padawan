1c1c02871345b0765c4b8465c3d0932b1ad2e81a
Sreejith S <sreejith2904@gmail.com>
2016-04-21 11:24:48 +0530

    add conditions

---
+++ progfun/conditions.scala
@@ -0,0 +1,35 @@
+
+
+
+object  Conditionals {
+
+
+  def and(arg1 : Boolean, arg2 : Boolean): Boolean  =   if(arg1) arg2 else False
+
+
+
+
+ def main(args: Array[String]) = {
+
+
+
+   def loop : Boolean = loop
+
+
+   // nothing happens, value not evaluated now
+   def x = loop
+
+
+   //repl dies, goes into loop at this point
+   //val x = loop
+
+
+   
+
+
+
+
+ }
+
+
+}
