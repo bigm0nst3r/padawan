b8ee6b6d741acb5dfaee7afbd9372a19967f28e6
Sreejith S <sreejith.sreekumar@247-inc.com>
2016-06-06 20:30:17 +0530

    animal shelter initial commit

---
+++ animal-shelter/xgb-train.py
@@ -0,0 +1,13 @@
+
+
+import pandas as pd
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import pdb 
+
+train_df = pd.read_csv('train.csv', header=0)
+test_df = pd.read_csv('test.csv', header=0)
+
+pdb.set_trace()
+
--- digit-rec/script.py
+++ digit-rec/script.py
@@ -1,6 +1,8 @@
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
+import pdb
+
 
 dataset = pd.read_csv("train.csv")
 target = dataset[[0]].values.ravel()
@@ -11,6 +13,7 @@ test = pd.read_csv("test.csv").values
 rf = RandomForestClassifier(n_estimators = 100)
 rf.fit(train, target)
 pred = rf.predict(test)
+pdb.set_trace()
 
 np.savetxt('submission_rand_forest.csv', np.c_[range(1,len(test)+1),pred], delimiter=',', header = 'ImageId,Label', comments = '', fmt='%d')
 
