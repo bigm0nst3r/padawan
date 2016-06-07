0dda2bc9c0518b770096cebb17d56cd3adcf5d99
Sreejith S <sreejith.sreekumar@247-inc.com>
2016-06-08 00:15:52 +0530

    finish script for animal shelter - One hot encode output to be done

--- animal-shelter/xgb-train.py
+++ animal-shelter/xgb-train.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
-import numpy as np
 import types
 
 train_df = pd.read_csv('train.csv', header=0)
 test_df = pd.read_csv('test.csv', header=0)
 
 
-featuresToUse = ["OutcomeType", "OutcomeSubtype", "AnimalType", "SexuponOutcome", "AgeuponOutcome", "Breed","Color"]
-nonNumericColumns = ["OutcomeType", "OutcomeSubtype", "AnimalType", "SexuponOutcome", "Breed", "Color"]
+featuresToUse = ["OutcomeType", "AnimalType", "SexuponOutcome", "AgeuponOutcome", "Breed","Color"]
+testFeatures = ["AnimalType", "SexuponOutcome", "AgeuponOutcome", "Breed","Color"]
+
+nonNumericColumnsTrain = ["OutcomeType", "OutcomeSubtype", "AnimalType", "SexuponOutcome", "Breed", "Color"]
+nonNumericColumnsTest = ["AnimalType", "SexuponOutcome", "Breed", "Color"]
 
 
 # checking if there are nulls in age
@@ -34,13 +36,57 @@ def year_removal(x):
     return '{0:.2f}'.format(y)
 
 train_df["AgeuponOutcome"] = train_df["AgeuponOutcome"].apply(year_removal)
+test_df["AgeuponOutcome"] = test_df["AgeuponOutcome"].apply(year_removal)
 
 
 # converting non numberic features to numeric
 le = LabelEncoder()
-for feature in nonNumericColumns :
+for feature in nonNumericColumnsTrain :
     train_df[feature] = le.fit_transform(train_df[feature])
+for feature in nonNumericColumnsTest :
+    test_df[feature] = le.fit_transform(test_df[feature])
 
 
+train_df = train_df[featuresToUse]
+test_df = test_df[testFeatures]
 print "Done..."
-    
+
+features = ["AnimalType", "SexuponOutcome", "AgeuponOutcome", "Breed","Color"]
+labels = ["OutcomeType"]
+
+trainData = train_df[features]
+trainLabels = train_df[labels]
+
+testData = test_df[features]
+
+
+trainData['AnimalType'] = trainData['AnimalType'].astype(float)
+trainData['SexuponOutcome'] = trainData['SexuponOutcome'].astype(float)
+trainData['AgeuponOutcome'] = trainData['AgeuponOutcome'].astype(float)
+trainData['Breed'] = trainData['Breed'].astype(float)
+trainData['Color'] = trainData['Color'].astype(float)
+
+
+testData['AnimalType'] = testData['AnimalType'].astype(float)
+testData['SexuponOutcome'] = testData['SexuponOutcome'].astype(float)
+testData['AgeuponOutcome'] = testData['AgeuponOutcome'].astype(float)
+testData['Breed'] = testData['Breed'].astype(float)
+testData['Color'] = testData['Color'].astype(float)
+
+gbm = xgb.XGBClassifier(max_depth=3, n_estimators=300, learning_rate=0.05).fit(trainData, trainLabels)
+print gbm
+print "Model Building..."
+
+predictions = gbm.predict(testData)
+submission = pd.DataFrame({ 'Prediction': predictions})
+
+
+def change_format(x):
+    _map = {0: 'Return_to_owner', 1:'Euthanasia', 2: 'Adoption', 3: 'Transfer', 4:'Died'}
+    return _map[x]
+
+submission["Reason"] = submission["Prediction"].apply(change_format)
+
+
+
+
